5231c389a57b4d5f91d4cc2a8a43f7c50d61cf55
andrijmih <an12dre34@rambler.ru>
2020-11-08 15:47:39 +0200

    feat: add webpack

---
+++ src/script.js
@@ -0,0 +1,10 @@
+import './module'
+
+const item = document.querySelector(".item");
+const item1 = document.querySelector(".item-i");
+
+item.addEventListener('click', function () {
+    // item.style.left = 50 + 'px';
+    item1.style.order = 1;
+    item.style.order = 2;
+});
